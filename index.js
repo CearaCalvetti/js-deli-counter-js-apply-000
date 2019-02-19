5de049e34f0c7da50650747e568fddc46ca7aea6
Ceara Calvetti <calvetti.ceara@gmail.com>
2019-02-19 20:11:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber(cLOP, newPersonName){
   var welcomeMessage = "Welcome, " + newPersonName + ". You are number " + cLOP.length + " in line."
   return welcomeMessage;
 }
+
+funcion nowServing(cLOP){
+var firstPerson = cLOP[0];
+cLOP.shift(firstPerson);
+if (cLOP.length === 0){return "There is nobody waiting to be served!"}
+else {return firstPerson}
+}
